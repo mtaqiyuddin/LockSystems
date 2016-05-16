faf5ce207965bf7910e6512ec12ce8cb071cdafa
Dito Augusta <dt_ap@ymail.com>
2016-05-16 19:42:13 +0700

    Add Arduino

---
+++ LED_Write_Read/LED_Write_Read.ino
@@ -0,0 +1,72 @@
+char command;
+String string;
+boolean ledon = false;
+#define led 5
+
+  void setup()
+  {
+    Serial.begin(9600);
+    pinMode(led, OUTPUT);
+  }
+
+  void loop()
+  {
+    if (Serial.available() > 0) 
+    {string = "";}
+    
+    while(Serial.available() > 0)
+    {
+      command = ((byte)Serial.read());
+      
+      if(command == ':')
+      {
+        break;
+      }
+      
+      else
+      {
+        string += command;
+      }
+      
+      delay(1);
+    }
+    
+    if(string == "1")
+    {
+        ledOn();
+        ledon = true;
+        Serial.println("lock");
+    }
+    
+    if(string =="0")
+    {
+        ledOff();
+        ledon = false;
+        Serial.println("unlock");
+    }
+    
+    if ((string.toInt()>=0)&&(string.toInt()<=255))
+    {
+      if (ledon==true)
+      {
+        analogWrite(led, string.toInt());
+        Serial.println(string);
+        delay(10);
+      }
+    }
+ }
+ 
+void ledOn()
+   {
+      analogWrite(led, 255);
+      delay(10);
+    }
+ 
+ void ledOff()
+ {
+      analogWrite(led, 0);
+      delay(10);
+ }
+ 
+
+    
